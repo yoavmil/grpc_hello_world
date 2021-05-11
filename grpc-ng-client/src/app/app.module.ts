6b98e4a57739018d465b4bba186b21fe3d5b05c5
Yoav Miller <yoavm@magnusmetal.com>
2021-05-11 14:45:42 +0300

    three.js hello, cube!

--- grpc-ng-client/src/app/app.module.ts
+++ grpc-ng-client/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ViewerComponent } from './viewer/viewer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewerComponent
   ],
   imports: [
     BrowserModule
